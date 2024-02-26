1d30aa1e7cacc7295f6564b6684720a78bcf1ebf
Sonam Ligade <sonamligade@gmail.com>
2024-02-26 15:17:32 +0530

    added react router practice project

---
+++ 12. react-router-practice/frontend/src/pages/EditEventPage.js
@@ -0,0 +1,6 @@
+
+const EditEventPage = () => {
+    return <h1>Welcome to EditEventPage</h1>
+};
+
+export default EditEventPage;
---
+++ 12. react-router-practice/frontend/src/pages/NewEventPage.js
@@ -0,0 +1,6 @@
+
+const NewEventPage = () => {
+    return <h1>Welcome to NewEventPage</h1>
+};
+
+export default NewEventPage;
